4e9454ff8a6f93c05d919328ad18a9910ebdae35
unknown <jrbpfilho@gmail.com>
2021-05-13 18:44:09 -0300

    Desenvolvimento Reativo - Projeto em Angular para IGTI - Simples cadastro de produtos

---
+++ src/app/produtos/form/form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Produto } from '../shared/produto';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  formProduto: FormGroup;
+  produtos: any = [];
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createForm(new Produto());
+  }
+
+  createForm(produto: Produto) {
+    this.formProduto = new FormGroup({
+      nome: new FormControl(produto.nome),
+      categoria: new FormControl(produto.categoria),
+      quantidade: new FormControl(produto.quantidade)
+    })
+  }
+  onSubmit() {
+    // aqui você pode implementar a logica para fazer seu formulário salvar
+    console.log(this.formProduto.value);
+    this.produtos.push(this.formProduto.value);
+    this.formProduto.reset(new Produto());
+  }
+
+}
